1d20c016bafc4ce03c5a10da8a4d19f4364cb0b6
Dark_Xen <davipa2004@gmail.com>
2025-03-28 13:56:37 +0100

    Cambios: - Añadidos los botones de la partida (solo visual sin funcionalidad) - Añadidas las barras de vida (solo visual sin funcionalidad)

---
+++ app/src/main/java/com/example/tfg_mansionzombie/Jugador.kt
@@ -0,0 +1,9 @@
+package com.example.tfg_mansionzombie
+
+class Jugador {
+    val vida: Int = 100
+    val daño: Int = 15
+    val protecciones: Int = 0
+    val curaciones: Boolean = false
+
+}
--- app/src/main/java/com/example/tfg_mansionzombie/PartidaActivity.kt
+++ app/src/main/java/com/example/tfg_mansionzombie/PartidaActivity.kt
@@ -17,6 +17,5 @@ class PartidaActivity : AppCompatActivity() {
             insets
         }
 
-
     }
 }
